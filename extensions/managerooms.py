d76491b47d4e285febee0ee24f3bb60f78f807ac
eye-sigil <ustin.a.h+github@gmail.com>
2018-02-12 12:30:34 -0500

    More work on Room model, managerooms

--- extensions/managerooms.py
+++ extensions/managerooms.py
@@ -1,14 +1,21 @@
 import discord
+from discord import commands
 import traceback
 import peony
 import models
 
 
 class ManageRooms:
+
     def __init__(self, bot):
         self.bot = bot
         self.conn = bot.conn
 
-    def create_room(self, user: discord.User, name: str,
-                    game: str=None, nsfw: bool=False, **users) -> models.Room:
-        ...
+    @commands.command()
+    def create_room(self, owner: discord.User, name: str,
+                    game: str=None, nsfw: bool=False,
+                    **members: discord.User) -> models.Room:
+        room = models.Room(
+            members, owner=owner, name=name, game=game, nsfw=nsfw)
+
+        return room
--- models/room.py
+++ models/room.py
@@ -1,15 +1,36 @@
 """Room Object for @rcade"""
 
 import discord
+import rethinkdb
 
 
 class Room:
     """Represents a private room on @rcade"""
 
     def _init_(self, owner: discord.User, name: str=None,
-               game: str=None, nsfw: bool=False, **users):
+               game: str=None, is_nsfw: bool=False,
+               is_private: bool=True, has_voice: bool=True,
+               image: str=None, cover: str=None, *members: discord.User):
         self.owner = owner
         if name:
             self.name = name
         else:
-            self.name = f"{owner.name}'s Room'"
+            self.name = f"{owner.name}'s Room"
+        self.game = game
+        self.is_nsfw = is_nsfw
+        self.is_private = is_private
+        self.has_voice = has_voice
+        if image:
+            self.image = image
+        else:
+            self.image = owner.avatar
+        self.cover = cover
+        self.members = [member for member in members]
+
+    async def delete(self):
+        """Deletes room."""
+        ...
+
+    async def add_members(self, *members: discord.User):
+        """Adds members to room."""
+        ...
