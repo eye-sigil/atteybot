3b3b453184bdaa1cf21014e2da9eaef09833bd12
eye-sigil <ustin.a.h+github@gmail.com>
2018-02-12 13:11:15 -0500

    Additions to room, Creation of panel model

---
+++ models/panel.py
@@ -0,0 +1,11 @@
+"""Panel Model"""
+
+import discord
+from discord import commands
+
+
+class Panel:
+    """Represents a panel channel in an @rcade room."""
+
+    def __init__(self, name: str="room"):
+        self.name = name
--- models/room.py
+++ models/room.py
@@ -1,7 +1,11 @@
-"""Room Object for @rcade"""
+"""Room Model"""
 
 import discord
 import rethinkdb
+import typing as t
+from panel import Panel
+
+UserList = t.Optional[t.List[discord.User]]
 
 
 class Room:
@@ -11,26 +15,50 @@ class Room:
                game: str=None, is_nsfw: bool=False,
                is_private: bool=True, has_voice: bool=True,
                image: str=None, cover: str=None, *members: discord.User):
+
+        # Basic Setup
         self.owner = owner
         if name:
             self.name = name
         else:
             self.name = f"{owner.name}'s Room"
         self.game = game
+        self.members = [member for member in members]
+
+        # Settings
         self.is_nsfw = is_nsfw
         self.is_private = is_private
         self.has_voice = has_voice
+
+        # Cosmetic
         if image:
             self.image = image
         else:
             self.image = owner.avatar
-        self.cover = cover
-        self.members = [member for member in members]
+        self.cover = cover  # Unimplemented Patreon Feature
+
+        # Components
+        self.panels = [Panel()]
+        if game:
+            self.panels.append(Panel(game))
+        self.groups = {}
 
     async def delete(self):
         """Deletes room."""
         ...
 
-    async def add_members(self, *members: discord.User):
+    async def add_members(self, *members: discord.User) -> UserList:
         """Adds members to room."""
+        ...
+
+    async def change_game(self):
+        """Changes room's game."""
+        ...
+
+    async def change_name(self, name: str=None):
+        """Changes the name of the room."""
+        ...
+
+    async def manage_groups(self, **groups: UserList):
+        """Manages game-based team or user channels."""
         ...
