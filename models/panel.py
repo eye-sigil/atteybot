582b518e66f58c7909b531ddbeb962edaa46c52f
eye-sigil <ustin.a.h+github@gmail.com>
2018-02-12 15:39:20 -0500

    Working on Panel

--- models/panel.py
+++ models/panel.py
@@ -1,18 +1,19 @@
 """Panel Model"""
 
 import discord
-from discord import commands
 
 
 class Panel(discord.TextChannel):
     """Represents a panel channel in an @rcade room."""
 
-    def __init__(self, name: str="room"):
+    def __init__(self, channel: discord.TextChannel, name: str="room"):
+        super.__init__()
         self.name = name
 
-    def add_setting():
+    def add_setting(self):
         """Adds setting to panel and channel."""
         ...
 
-    def delete():
-        
+    async def delete(self):
+        """Deletes panel and relevant rethinkdb entries."""
+        super().delete()
